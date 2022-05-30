81b1f7d6c8f40fded84e3bc28a191544c5a8fdff
theshatterstone <agerenski@hotmail.com>
2022-05-30 09:59:51 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.title('TKST text editor')
+#all the code goes here
+Label(root, text='This is the area, where I will build a text editor.').grid(row=0, column=0, sticky='e') #filler label, so the window isn't empty
+
+
+root.mainloop()
